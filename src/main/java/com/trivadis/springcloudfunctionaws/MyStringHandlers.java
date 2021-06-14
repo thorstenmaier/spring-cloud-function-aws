a35ab443fc1d4e994dbdf5f56ba899574427faa8
Thorsten Maier <Thorsten.Maier@trivadis.com>
2021-06-14 09:53:08 +0200

    Initial AWS deployment - working!

---
+++ src/main/java/com/trivadis/springcloudfunctionaws/MyStringHandlers.java
@@ -0,0 +1,7 @@
+package com.trivadis.springcloudfunctionaws;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class MyStringHandlers extends SpringBootRequestHandler<String, String> {
+
+}
